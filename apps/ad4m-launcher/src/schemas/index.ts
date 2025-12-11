0d614a723b77eba30763f65780f890ad971e304d
jhweir <james@weco.io>
2025-12-11 12:47:47 +0000

    Pnpm lockfile updates

--- apps/ad4m-launcher/src/schemas/index.ts
+++ apps/ad4m-launcher/src/schemas/index.ts
@@ -1,5 +1,4 @@
 export { defaultTemplateSchema } from './DefaultTemplate.schema';
-export { secondaryTemplateSchema } from './SecondaryTemplate.schema';
 export { twitterTemplateSchema } from './TwitterTemplate.schema';
 export { aiSampleTemplateSchema } from './BaseTemplate.schema';
 export { testTemplateSchema } from './TestTemplate.schema';
